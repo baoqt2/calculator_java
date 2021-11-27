4aa0db90e4d5ee2f7cc34c62d5a2c3aa37abfdd9
baoqt2 <baoqt2@uci.edu>
2021-11-27 15:28:36 -0800

    submitted

--- GUI_Calculator_Assignment/GUI_Calculator_Assignment/src/GUI_Calculator_Assignment.java
+++ GUI_Calculator_Assignment/GUI_Calculator_Assignment/src/GUI_Calculator_Assignment.java
@@ -1,170 +1,237 @@
 //Ann Trac
 // package myJavaFXpkg;
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-//import javafx.scene.layout.Border;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-/*import javafx.scene.text.TextAlignment;*/
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-import javax.swing.JOptionPane;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.lang.model.util.ElementScanner6;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
 import java.io.IOException;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class GUI_Calculator_Assignment extends Application 
+
+public class GUI_Calculator_Assignment extends JFrame
 {
-	@Override // Override the start method in the Application class
-	public void start(Stage primaryStage) throws Exception
-	{
-		double sum = 0.0;
-	// Create a scene and place a button in the scene.
-	Pane pane = new Pane();
-	final JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-		
-		Label myLabel = new Label("123456");
-		myLabel.setFont(Font.font("Courier",FontWeight.BOLD,FontPosture.REGULAR,20));
-		myLabel.setStyle("-fx-border-color: blue;");
-		myLabel.setAlignment(Pos.BASELINE_RIGHT); // align text to the right side of the label.
-		myLabel.setLayoutX(10);       // set the x location of the label
-		myLabel.setLayoutY(10);       // set the y location of the label
-		myLabel.setPrefSize(250, 20); // set the width and height of the label
-		myLabel.setText("123456789"); // put some numbers into the label
-		pane.getChildren().add(myLabel);
-		myLabel.setText(Double.toString(sum));
-		
-		//creating a GridPane for buttons
-		GridPane gridPane = new GridPane();
-		gridPane.setHgap(5);
-		gridPane.setVgap(5);
-		gridPane.setAlignment(Pos.CENTER);
-		gridPane.setPrefSize(250,100);
-		gridPane.setLayoutX(10);
-		gridPane.setLayoutY(80);
-		
-		//creating buttons in Calculator
-		//First row
-		Button sevenButton = new Button("7");
-		gridPane.add(sevenButton, 0, 0);
-		Button eightButton = new Button("8");
-		gridPane.add(eightButton, 1, 0);
-		Button nineButton = new Button("9");
-		gridPane.add(nineButton, 2, 0);
-		Button divideButton = new Button("/");
-		gridPane.add(divideButton,3, 0);
-		//Second Row
-		Button fourButton = new Button("4");
-		gridPane.add(fourButton, 0, 1);
-		Button fiveButton = new Button("5");
-		gridPane.add(fiveButton, 1, 1);
-		Button sixButton = new Button("6");
-		gridPane.add(sixButton, 2, 1);
-		Button multiplyButton = new Button("x");
-		gridPane.add(multiplyButton, 3, 1);
-		//Third Row
-		Button oneButton = new Button("1");
-		gridPane.add(oneButton, 0, 2);
-		Button twoButton = new Button("2");
-		gridPane.add(twoButton, 1, 2);
-		Button threeButton = new Button("3");
-		gridPane.add(threeButton, 2, 2);
-		Button subtractButton = new Button("-");
-		gridPane.add(subtractButton, 3, 2);
-		//Fourth Row
-		Button zeroButton = new Button("0");
-		gridPane.add(zeroButton, 0, 3);
-		Button decimalButton = new Button(".");
-		gridPane.add(decimalButton, 1, 3);
-		Button additionButton = new Button("+");
-		gridPane.add(additionButton, 2, 3);
-		Button equalButton = new Button("=");
-		gridPane.add(equalButton, 3, 3);
-		//Button sizes
-		additionButton.setPrefSize(50, 30);
-        subtractButton.setPrefSize(50, 30);
-        multiplyButton.setPrefSize(50, 30);
-        divideButton.setPrefSize(50, 30);
-        zeroButton.setPrefSize(50, 30);
-        oneButton.setPrefSize(50, 30);
-        twoButton.setPrefSize(50, 30);
-        threeButton.setPrefSize(50, 30);
-        fourButton.setPrefSize(50, 30);
-        fiveButton.setPrefSize(50, 30);
-        sixButton.setPrefSize(50, 30);
-        sevenButton.setPrefSize(50, 30);
-        eightButton.setPrefSize(50, 30);
-        nineButton.setPrefSize(50, 30);
-        equalButton.setPrefSize(50, 30);
-		decimalButton.setPrefSize(50, 30);
-		//Font style
-		additionButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        subtractButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        multiplyButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        divideButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        zeroButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        oneButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        twoButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        threeButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        fourButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        fiveButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        sixButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        sevenButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        eightButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        nineButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-        equalButton.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,14));
-		pane.getChildren().add(gridPane);
+	private JButton oneButton = new JButton("1");
+	private JButton twoButton = new JButton("2");
+	private JButton threeButton = new JButton("3");
+	private JButton fourButton = new JButton("4");
+	private JButton fiveButton = new JButton("5");
+	private JButton sixButton = new JButton("6");
+	private JButton sevenButton = new JButton("7");
+	private JButton eightButton = new JButton("8");
+	private JButton nineButton = new JButton("9");
+	private JButton zeroButton = new JButton("0");
+	private JButton decButton = new JButton(".");
+	private JButton addButton = new JButton("+");
+	private JButton subButton = new JButton("-");
+	private JButton mulButton = new JButton("*");
+	private JButton divButton = new JButton("/");
+	private JButton equalButton = new JButton("=");
+	private JButton resetButton = new JButton("Reset");
 
+	private JTextField result_field = new JTextField("");
+	String display = "";
+	int counter = 0;
+	double total,temp = 0.0;
+	String last_operator = "";
 
-	//	Button addButton = new Button();
+	public GUI_Calculator_Assignment () {
+		double result = 0.0;
+		JPanel p1 = new JPanel(new GridLayout(5,2));
+		p1.add(sevenButton);
+		p1.add(eightButton);
+		p1.add(nineButton);
+		p1.add(divButton);
+		p1.add(fourButton);
+		p1.add(fiveButton);
+		p1.add(sixButton);
+		p1.add(mulButton);
 
-		
-		Scene scene = new Scene(pane,280,300);
-		primaryStage.setTitle("MyJavaFX"); // Set the stage title
-		primaryStage.setScene(scene); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
+		p1.add(oneButton);
+		p1.add(twoButton);
+		p1.add(threeButton);
+		p1.add(subButton);
+
+		p1.add(zeroButton);
+		p1.add(decButton);
+		p1.add(addButton);
+		p1.add(equalButton);
+		JPanel p2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		p2.add(result_field);
+		p2.add(resetButton);
+
+		p1.add(result_field);
+        zeroButton.setSize(50,30);
+
+		p1.setBorder(new TitledBorder("Thu's Calculator"));
+		add(p1, BorderLayout.CENTER);
+		add(p2,BorderLayout.SOUTH);
+		oneButton.addActionListener(new ButtonListener());
+		twoButton.addActionListener(new ButtonListener());
+		threeButton.addActionListener(new ButtonListener());
+		fourButton.addActionListener(new ButtonListener());
+		fiveButton.addActionListener(new ButtonListener());
+		sixButton.addActionListener(new ButtonListener());
+		sevenButton.addActionListener(new ButtonListener());
+		eightButton.addActionListener(new ButtonListener());
+		nineButton.addActionListener(new ButtonListener());
+		zeroButton.addActionListener(new ButtonListener());
+		decButton.addActionListener(new ButtonListener());
+		addButton.addActionListener(new ButtonListener());
+		subButton.addActionListener(new ButtonListener());
+		mulButton.addActionListener(new ButtonListener());
+		divButton.addActionListener(new ButtonListener());
+		equalButton.addActionListener(new ButtonListener());
+		resetButton.addActionListener(new ButtonListener());
 	}
 
-	public void calculation(int number1, int number2, string operator)
-	{
-		float result = 0;
-
-		switch (operator){
-			case "+":
-				result = number1 + number2;
-				break;
-
-			case "-":
-				result = number1 - number2;
-				break;
-
-			case "x":
-				result = number1 * number2;
-				break;
-
-			case "/":
-				try{
-					if (number2 != 0)
-						result = number1 / number2;	
-				} catch (IOException e){
-					JOptionPane.showMessageDialog (null, "Something went Wrong", "Title", JOptionPane.ERROR_MESSAGE);
-                //LOGGER.error("Error message: " + e.getMessage());
-				}
-			default:
-				break;
+	private class ButtonListener implements ActionListener {
+		
+		public int str_to_int(String num) {
+			Integer stringtoInteger = Integer.parseInt(num);
+        	return stringtoInteger.intValue();
 		}
+	
+		public void get_result(String num_string, String last_operator) {
+			double num = Integer.parseInt(num_string);
+			switch (last_operator){
+				case "+":
+					total += num;
+					break;
+	
+				case "-":
+					if (total == 0) 
+						total = num;
+					else
+						total -= num;
+					break;
+	
+				case "*":
+				if (total == 0) 
+					total = num;
+				else
+					total *= num;
+				break;
+	
+				case "/":
+					if (total == 0) 
+						total = num;
+					else
+						total /= num;	
+					break;
+				default:
+					break;
+			}
+			
+	
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+					
+			if (e.getSource() == oneButton) {
+				display += "1";
+				result_field.setText(display);
 
+//				oneButton.addActionListener(new ButtonListener());
+			}
+			else if (e.getSource() == twoButton) {
+				display += "2";
+				result_field.setText(display);
+
+//				twoButton.addActionListener(new ButtonListener());
+			}
+			else if (e.getSource() == threeButton) {
+				display += "3";
+				result_field.setText(display);
+
+//				threeButton.addActionListener(new ButtonListener());
+
+			}
+			else if (e.getSource() == fourButton) {
+				display += "4";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == fiveButton) {
+				display += "5";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == sixButton) {
+				display += "6";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == sevenButton) {
+				display += "7";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == eightButton) {
+				display += "8";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == nineButton) {
+				display += "9";
+				result_field.setText(display);
+
+			}
+			else if (e.getSource() == zeroButton) {
+				display += "0";
+				result_field.setText(display);
+			}
+			else if (e.getSource() == equalButton) {
+				result_field.setText(display);				
+				get_result(display, last_operator);				
+				result_field.setText(Double.toString(total));
+				display="";
+			}
+			else if (e.getSource() == addButton)	{
+				last_operator = "+";
+				get_result(display, last_operator);
+				display="";
+				result_field.setText(Double.toString(total));
+			}
+			else if (e.getSource() == subButton)	{
+				last_operator = "-";
+				get_result(display, last_operator);
+				display="";
+				result_field.setText(Double.toString(total));
+			}
+			else if (e.getSource() == mulButton)	{
+				last_operator = "*";
+				get_result(display, last_operator);
+				display="";
+				result_field.setText(Double.toString(total));
+			}
+			else if (e.getSource() == divButton)	{
+				last_operator = "/";
+				get_result(display, last_operator);
+				display="";
+				result_field.setText(Double.toString(total));
+			}
+			else if (e.getSource() == resetButton){
+				last_operator = "";
+				total = 0;
+				temp = 0;
+				display = "";
+				result_field.setText(Double.toString(total));
+
+			}
+
+
+		}
 	}
 
 	public static void main(String[] args){
-
-		Application.launch(args);
+		GUI_Calculator_Assignment frame = new GUI_Calculator_Assignment();
+		frame.pack();
+		frame.setTitle("Calculator");
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
